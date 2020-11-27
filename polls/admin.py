ff9ef562c5fbacfe9e0a4d37cf08ea55099a850e
Derek <derek.thefox@icloud.com>
2020-11-28 03:49:04 +0900

    Question Admin 메뉴의 순서 변경
    Question Admin 메뉴의 순서 변경

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']    # 빌드 순서 변경
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+
+admin.site.register(Question, )
