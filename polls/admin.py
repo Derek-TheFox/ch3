66e07a3baf5401cfd961230ad23172473d9e60cc
Derek <derek.thefox@icloud.com>
2020-11-29 15:23:51 +0900

    admin 메뉴 개선
    1. 각 필드를 분리해서 보여주기
    2. Data Information 필드 '접기' 기능 추가

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']    # 빌드 순서 변경
+    fields = ['pub_date', 'question_text']    # 필드순서 변경
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Statement', {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-
-admin.site.register(Question, )
