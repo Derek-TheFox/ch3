2dbeb27d45139058458eb9b81e9ae56870868b37
Derek <derek.thefox@icloud.com>
2020-12-03 04:46:50 +0900

    Admin 메뉴에서 field 편집방식 변경
    개별입력 -> 복수 데이터 입력
    가능하도록 TabularInline 방식으로 옵션 추가

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 from polls.models import Question, Choice
-
+'''
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']    # 필드순서 변경
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Question Statement', {'fields': ['question_text']}),
-        ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
+        ('Question Statement',  {'fields': ['question_text']}),
+        ('Date Information',    {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
+'''
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields':['question_text']}),
+        ('Date information',    {'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]   # Choice 모델 클래스 같이 보기
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
